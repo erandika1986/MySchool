d6271bfe1c103ba276e074cdb3fccc6ca08457bb
Avdunusinghe <avdunusinghe@gmail.com>
2022-09-29 20:08:56 +0530

    Db Configuration Partially Implemented

---
+++ src/Infrastructure/MySchool.Infrastructure/Data/DbConnector.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace MySchool.Infrastructure.Data
+{
+    public class DbConnector
+    {
+        private readonly IConfiguration _configuration;
+
+        protected DbConnector(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+    }
+}
---
+++ src/Infrastructure/MySchool.Infrastructure/Data/MySchoolContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MySchool.Infrastructure.Data
+{
+    public class MySchoolContext : DbContext
+    {
+        public MySchoolContext(DbContextOptions<MySchoolContext> options) : base(options)
+        {
+
+        }
+    }
+}
