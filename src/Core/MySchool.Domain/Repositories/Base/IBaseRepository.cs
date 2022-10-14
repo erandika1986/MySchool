547df2c8d0383c083289a7befaa5cd5486676ab7
rishandileepa <rishdil321@gmail.com>
2022-10-14 06:44:53 +0530

    Base & Core repository interfaces created

---
+++ src/Core/MySchool.Domain/Repositories/Base/IBaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySchool.Domain.Repositories.Base
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        Task<T> CreateAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<bool> DeleteAsync(T entity);
+        Task<List<T>> GetAll();
+        Task<T> GetById(int id);
+    }
+}
---
+++ src/Core/MySchool.Domain/Repositories/IGradeRepository.cs
@@ -0,0 +1,15 @@
+﻿using MySchool.Domain.Entities;
+using MySchool.Domain.Entities.School;
+using MySchool.Domain.Repositories.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySchool.Domain.Repositories
+{
+    public interface IGradeRepository : IBaseRepository<Grade>
+    {
+    }
+}
