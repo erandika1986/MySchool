dc364bdb5fe29e0204ba7424c9ffb736f0545eda
Erandika <erandika@solistech.com>
2022-10-26 16:09:42 +0530

    Repository pattern partially implemented.

---
+++ src/Infrastructure/MySchool.Infrastructure/Repositories/Base/BaseRepository.cs
@@ -0,0 +1,48 @@
+﻿using MySchool.Domain.Repositories.Base;
+using MySchool.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySchool.Infrastructure.Repositories.Base
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        protected readonly MySchoolContext _context;
+
+        public BaseRepository(MySchoolContext context)
+        {
+            this._context = context;
+        }
+
+        public async Task<T> CreateAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public Task<bool> DeleteAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<T>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
