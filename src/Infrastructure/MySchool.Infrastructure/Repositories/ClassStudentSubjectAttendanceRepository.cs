ca9d4fe6602c0543d9b6960d43ffb99c589bd0f0
rishandileepa <rishdil321@gmail.com>
2022-10-27 15:16:11 +0530

    Define Repository classes

---
+++ src/Infrastructure/MySchool.Infrastructure/Repositories/ClassStudentSubjectAttendanceRepository.cs
@@ -0,0 +1,22 @@
+﻿using MySchool.Domain.Entities;
+using MySchool.Domain.Entities.School;
+using MySchool.Domain.Repositories;
+using MySchool.Infrastructure.Data;
+using MySchool.Infrastructure.Repositories.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySchool.Infrastructure.Repositories
+{
+    public class ClassStudentSubjectAttendanceRepository : BaseRepository<ClassStudentSubjectAttendance>, IClassStudentSubjectAttendanceRepository
+    {
+        public ClassStudentSubjectAttendanceRepository(MySchoolContext context)
+            : base(context)
+        {
+
+        }
+    }
+}
---
+++ src/Infrastructure/MySchool.Infrastructure/Repositories/SubjectTeacherRepository.cs
@@ -0,0 +1,22 @@
+﻿using MySchool.Domain.Entities.School;
+using MySchool.Domain.Repositories;
+using MySchool.Infrastructure.Data;
+using MySchool.Infrastructure.Repositories.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySchool.Infrastructure.Repositories
+{
+    public class SubjectTeacherRepository : BaseRepository<SubjectTeacher>, ISubjectTeacherRepository
+    {
+        public SubjectTeacherRepository(MySchoolContext context)
+            : base(context)
+        {
+
+        }
+    }
+}
+
