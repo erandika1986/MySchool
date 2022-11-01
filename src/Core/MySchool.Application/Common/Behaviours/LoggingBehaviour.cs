0d9af1fcb2155b69a876326d70bd63be616f2c2e
Erandika <erandika@solistech.com>
2022-11-01 11:40:12 +0530

    Behaviours and Custom Exception classes added.

--- src/Core/MySchool.Application/Common/Behaviours/LoggingBehaviour.cs
+++ src/Core/MySchool.Application/Common/Behaviours/LoggingBehaviour.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MediatR.Pipeline;
+using Microsoft.Extensions.Logging;
+using MySchool.Application.Common.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,24 @@ using System.Threading.Tasks;
 
 namespace MySchool.Application.Common.Behaviours
 {
-    internal class LoggingBehaviour
+    public class LoggingBehaviour<TRequest> : IRequestPreProcessor<TRequest> where TRequest : notnull
     {
+        private readonly ILogger _logger;
+        private readonly ICurrentUserService _currentUserService;
+
+        public LoggingBehaviour(ILogger<TRequest> logger, ICurrentUserService currentUserService)
+        {
+            _logger = logger;
+            _currentUserService = currentUserService;
+        }
+
+        public async Task Process(TRequest request, CancellationToken cancellationToken)
+        {
+            var requestName = typeof(TRequest).Name;
+            var userId = _currentUserService.UserId ?? 0;
+
+            _logger.LogInformation("Demo Request: {Name} {@UserId} {@Request}",
+                requestName, userId, request);
+        }
     }
 }
--- src/Core/MySchool.Application/Common/Behaviours/PerformanceBehaviour.cs
+++ src/Core/MySchool.Application/Common/Behaviours/PerformanceBehaviour.cs
@@ -1,12 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using MySchool.Application.Common.Interfaces;
+using System.Diagnostics;
 
 namespace MySchool.Application.Common.Behaviours
 {
-    internal class PerformanceBehaviour
+    public class PerformanceBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : MediatR.IRequest<TResponse>
     {
+        private readonly Stopwatch _timer;
+        private readonly ILogger<TRequest> _logger;
+        private readonly ICurrentUserService _currentUserService;
+        //private readonly IIdentityService _identityService;
+
+        public PerformanceBehaviour(
+            ILogger<TRequest> logger,
+            ICurrentUserService currentUserService
+         //IIdentityService identityService
+         )
+        {
+            _timer = new Stopwatch();
+
+            _logger = logger;
+            _currentUserService = currentUserService;
+            //_identityService = identityService;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            _timer.Start();
+
+            var response = await next();
+
+            _timer.Stop();
+
+            var elapsedMilliseconds = _timer.ElapsedMilliseconds;
+
+            if (elapsedMilliseconds <= 500) return response;
+            var requestName = typeof(TRequest).Name;
+            var userId = _currentUserService.UserId ?? 0;
+
+
+            _logger.LogWarning("Demo Long Running Request: {Name} ({ElapsedMilliseconds} milliseconds) {@UserId} {@Request}",
+                requestName, elapsedMilliseconds, userId, request);
+
+            return response;
+        }
     }
 }
